cacc80e205066a564d6e5a245a6c1d68b4b37560
Noel Vaca <noel.vm.2501@gmail.com>
2025-04-24 20:34:23 -0400

    feat: creación del proyecto demo_vue y ejemplo básico

---
+++ demo_vue/src/views/Prueba.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import ListaItem from '@/components/ListaItem.vue'
+import { ref } from 'vue'
+
+const titulo = ref<string>('Página de Prueba')
+const contador = ref<number>(0)
+const colores = ref([
+  { clave: 'red', valor: 'Rojo' },
+  { clave: 'yellow', valor: 'Amarillo' },
+  { clave: 'green', valor: 'Verde' },
+  { clave: 'blue', valor: 'Azul' },
+])
+</script>
+
+<template>
+  <div>
+    <h1>{{ titulo }} - {{ contador }}</h1>
+    <button @click="contador++">Incrementar</button>
+    Cambiar título: <input type="text" v-model="titulo" />
+    <p v-if="contador % 2 == 0">Es Par</p>
+    <p v-else>Es Impar</p>
+    <ul>
+      <ListaItem v-for="(color, i) in colores" :color="color" :key="i" />
+    </ul>
+  </div>
+</template>
+
+<style scoped></style>
